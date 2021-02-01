2502c1e9acdc101e36231c33713a584f886c3798
alla <allalenchner@gmail.com>
2021-02-01 15:20:54 +0200

    Adding cherry-pick to main.cpp
    (cherry picked from commit cb1a9b866e482f932d07205b0c3cf7e51dcaad17)

--- main.cpp
+++ main.cpp
@@ -3,5 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Here are some changes" << std::endl;
+    std::cout << "Some changes from master branch" << std::endl;
+    std::cout << "A change to cherry-pick" << std::endl;
     return 0;
 }
